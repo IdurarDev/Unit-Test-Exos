bf16eefd96f9dbc35f2cfd3e69fb1436b0a24fb2
Hamid Berkaine <hamiddev78@gmail.com>
2021-12-18 17:50:59 +0100

    add exo triangle

---
+++ katas/01_loop-triangle/loop.js
@@ -0,0 +1,8 @@
+// let triangle = ["#", "##", "###", "####", "#####", "######", "#######"];
+// for (let i = 0; i < triangle.length; i++) {
+//   console.log(triangle[i]);
+// }
+let tr = ''
+for (let i = 0; i < 7; i++) {
+  console.log(tr += '#')
+}
