fd7cb1eaee56d2decaf91263a36587fdfa0a77b5
Hamid Berkaine <hamiddev78@gmail.com>
2021-12-18 18:03:44 +0100

    add exo fizzbuzz in the kata folder

---
+++ katas/02_fizzbuzz/fizzbuzz.js
@@ -0,0 +1,28 @@
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz')
+  } else if (i % 3 === 0) {
+    console.log('Fizz')
+  } else if (i % 5 === 0) {
+    console.log('Buzz')
+  } else {
+    console.log(i)
+  } 
+}
+
+
+// function  fizzbuzz () {
+//   for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('FizzBuzz')
+//     } else if (i % 3 === 0) {
+//       console.log('Fizz')
+//     } else if (i % 5 === 0) {
+//       console.log('Buzz')
+//     } else {
+//       console.log(i)
+//     } 
+//   }
+// }
+
+// fizzbuzz()
