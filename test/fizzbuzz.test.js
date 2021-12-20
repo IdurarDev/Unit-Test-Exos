73334e387fcfe5c25ddbea191c733867eb5a7d4f
Hamid Berkaine <hamiddev78@gmail.com>
2021-12-20 14:33:42 +0100

    modif fizzbuzz.test file

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -4,7 +4,7 @@ import { fizzbuzz } from "../katas/02_fizzbuzz/fizzbuzz.js";
 
 describe('#fizzbuzz()', () => {
   it("Return a number that is divisible by 3", () => {
-    assert.equal(fizzbuzz(66), "Fizz", "A number is'nt divisible by 3")
+    assert.equal(fizzbuzz(66), "Fizz", "A number isn't divisible by 3")
   })
   it("Return a number that is divisible by 5", () => {
     assert.equal(fizzbuzz(5), "Buzz", "A number is'nt divisible by 5")
@@ -13,9 +13,9 @@ describe('#fizzbuzz()', () => {
     assert.equal(fizzbuzz(15),"FizzBuzz", "A number is'nt divisible by 3 and 5")
   })
   it("Return a string that is not divisible by 3 and 5", () => {
-    assert.equal(fizzbuzz(7), '7', "A number is'nt divisible by 3 and 5")
+    assert.equal(fizzbuzz(7), '7', "A number is not divisible by 3 and 5")
   })
   it("Return a number that is not divisible by 3 and 5", () => {
-    assert.notEqual(fizzbuzz(7), "Fizz", "A number is'nt divisible by 3 and 5")
+    assert.notEqual(fizzbuzz(7), "Fizz", "A number isn't divisible by 3 and 5")
   })
 })
