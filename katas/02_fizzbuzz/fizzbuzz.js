cc1dae2bc2011effd77fae03e2ae2317345aec92
Hamid Berkaine <hamiddev78@gmail.com>
2021-12-20 13:31:00 +0100

    modif fizzbuzz file in kata folder

--- katas/02_fizzbuzz/fizzbuzz.js
+++ katas/02_fizzbuzz/fizzbuzz.js
@@ -1,28 +1,25 @@
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz')
-  } else if (i % 3 === 0) {
-    console.log('Fizz')
-  } else if (i % 5 === 0) {
-    console.log('Buzz')
-  } else {
-    console.log(i)
-  } 
-}
-
-
-// function  fizzbuzz () {
-//   for (let i = 1; i <= 100; i++) {
-//     if (i % 3 === 0 && i % 5 === 0) {
-//       console.log('FizzBuzz')
-//     } else if (i % 3 === 0) {
-//       console.log('Fizz')
-//     } else if (i % 5 === 0) {
-//       console.log('Buzz')
-//     } else {
-//       console.log(i)
-//     } 
-//   }
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz')
+//   } else if (i % 3 === 0) {
+//     console.log('Fizz')
+//   } else if (i % 5 === 0) {
+//     console.log('Buzz')
+//   } else {
+//     console.log(i)
+//   } 
 // }
 
-// fizzbuzz()
+export function  fizzbuzz (i) {
+  let result = ''
+  if (i % 3 === 0 && i % 5 === 0) {
+    result += 'FizzBuzz'
+  } else if (i % 3 === 0) {
+    result += 'Fizz'
+  } else if (i % 5 === 0) {
+    result += 'Buzz'
+  } else {
+    result += i
+  } 
+  return result
+}
